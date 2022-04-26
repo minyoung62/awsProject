602548930adbbb8e34369dfff30f17af94c55e0f
minyoung62 <tnd05106@naver.com>
2022-04-26 22:40:59 +0900

    Hello api와 Test

---
+++ src/test/java/com/example/awsProject/HelloResponseDtoTest.java
@@ -0,0 +1,23 @@
+package com.example.awsProject;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloResponseDtoTest {
+
+    @Test
+    public void 룸복_기능_테스트() throws Exception {
+        //given
+        String name = "test";
+        int amount = 1000;
+
+        //when
+        HelloResponseDto dto = new HelloResponseDto(name, amount);
+
+        //then
+        assertThat(dto.getName()).isEqualTo(name);
+        assertThat(dto.getAmount()).isEqualTo(amount);
+    }
+}
